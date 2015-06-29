b9d673ef2511cabce22ac4fec7b9aa439ddaecdd
Shaiful Islam Palash <kuvic16@gmail.com>
2015-06-30 00:22:33 +0600

    new commit

---
+++ wp-content/plugins/ch2-plugin-header/ch2-plugin-header.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ Plugin Name: Chapter 2 - Plugin Header
+ Plugin URI:
+ Description: Declares a plugin that will be visible in the Wordpress admin interface
+ Version:     1.0
+ Author:      Shaiful Islam Palash<kuvic16@gmail.com> 
+ Author URI:  http://sheemoul.wordpress.com
+ License:     GPLv2
+ */
+?>
+
