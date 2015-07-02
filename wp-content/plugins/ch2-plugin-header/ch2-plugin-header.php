6223682933ea8a67ed19ff7ee0a2115c75fa5a3d
Shaiful Islam Palash <kuvic16@gmail.com>
2015-07-02 20:07:43 +0600

    new commit

--- wp-content/plugins/ch2-plugin-header/ch2-plugin-header.php
+++ wp-content/plugins/ch2-plugin-header/ch2-plugin-header.php
@@ -25,5 +25,21 @@ function ch2pho_page_header_output() { ?>
     </script>
 <?php
 }
+
+add_filter('the_content', 'ch2lfa_link_filter_analytics');
+function ch2lfa_link_filter_analytics($the_content){
+    $new_content = str_replace("href", "onClick=\"recordOutboundLink(this, 'Outbound Links','". home_url()."'); return false; \" href", $the_content);
+    return $new_content;
+}
+
+add_action('wp_footer', 'ch2lfa_footer_analytics_code');
+function ch2lfa_footer_analytics_code(){ ?>
+    <script type="text/javascript">
+        function recordOutboundLink(link, category, action){
+            _gat._getTrackerByName()._trackEvent(category, action);
+            setTimeout('document.location="'+ link.href + '"', 100);
+        }
+    </script>
+<?php }
 ?>
 
